3fb621916c7cad12922999e5575b8fcdfdaada3c
Sanjib Karmakar <sankarm1@in.ibm.com>
2020-08-31 19:28:43 +0530

    bookings added

--- app.js
+++ app.js
@@ -2,111 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const graphqlHttp = require('express-graphql');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+
+
 app.use(
     '/graphql',
     graphqlHttp({
-        schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-        rootValue: {
-            events: () => {
-                return Event.find()
-                    .then(events => {
-                        return events.map(event => {
-                            return { ...event._doc, _id: event._doc._id.toString() };
-                        });
-                    })
-                    .catch(err => {
-                        throw err;
-                    })
-            },
-            createEvent: args => {
-                const event = new Event({
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
-                });
-                return event
-                    .save().then(result => {
-                        console.log(result);
-                        return { ...result._doc, _id: result._doc._id.toString() };
-                    }).catch(err => {
-                        console.log(err);
-                        throw err;
-                    });
-            },
-            createUser: args => {
-                return User.findOne({ email: args.userInput.email }).then(user => {
-                    if (user) {
-                        throw new Error('User exists already.');
-                    }
-                    return bcrypt.hash(args.userInput.password, 12);
-                })
-                    .then(hashedPassword => {
-                        const user = new User({
-                            email: args.userInput.email,
-                            password: hashedPassword
-                        });
-                        return user.save();
-                    })
-                    .then(result => {
-                        return { ...result._doc, password: null, _id: result.id };
-                    })
-                    .catch(err => {
-                        throw err;
-                    });
-            }
-        },
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
         graphiql: true
     }));
 
@@ -117,3 +28,48 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA
     console.log(err);
 })
 
+
+
+// https://www.youtube.com/watch?v=bgq7FRSPDpI&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&index=7
+// http://localhost:3000/graphql
+
+// query {
+//     events {
+//       creator {
+//         email
+//       }
+//     }
+//   }
+
+
+// query {
+//     events {
+//       creator {
+//         email,
+//         createdEvents {
+//           title
+//           creator {
+//             email
+//           }
+//         }
+//       }
+//     }
+//   }
+
+// mutation {
+//     createEvent (eventInput: {title: "another event!", description:"asdasd", price: 999.99, date: "2020-08-16T06:18:16.179Z"}) {
+//       title
+      
+//     }
+    
+//   }
+
+// mutation {
+//     bookEvent(eventId: "5f391903a7ebb925f831d8af") {
+//       _id
+//       createdAt
+//       user {
+//         email
+//       }
+//     }
+//   }
